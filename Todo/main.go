af4e1d2c1f4954314c31ef6151846a1ee88d8465
disorn-inc <dirsorn1111@hotmail.com>
2021-12-01 20:22:36 +0700

    todo create api

---
+++ Todo/main.go
@@ -0,0 +1,23 @@
+package main
+
+import ( 
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"github/disorn-inc/Developing-Robust-API-Services-with-Go/Todo/todo" 
+)
+
+func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&todo.Todo{})
+	r := gin.Default()
+	
+	handler := todo.NewTodoHandler(db)
+	r.POST("/todos", handler.NewTask)
+	r.Run()
+}
